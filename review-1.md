2cee6f076bb404fe3ca39be50aa3a5b3dbd41257
Felipe Salazar <felipesalazar19@hotmail.com>
2023-07-11 22:00:16 -0500

    creacion project-2

--- review-1.md
+++ review-1.md
@@ -1,33 +1,48 @@
-# Lista de Tareas con Express.js
+# Lista de Tareas y Servidor con Node.js
 
-## ¿Qué es mi producto y para qué sirve?
-Nuestro producto es una aplicación para administrar tareas con un claro objetivo: permitir a los usuarios organizar su día a día. La lista de tareas le permite a los usuarios crear tareas nuevas, agregarles un identificador único, marcar tareas como completadas o dejarlas en pendiente y describir y especificar la tarea.
-La aplicación de lista de tareas con express es una clara muestra de que se pueden hacer cosas fáciles pero eficientes.
+## ¿Qué es el producto y para qué sirve?
+Una aplicación de list de tareas y de un servidor creados con Node.js. La aplicación te permite organizar tu agenda creando, eliminando y marcando como completas tus tareas.
 
-## ¿Cuáles son las funcionalidades más importantes y por qué los usuarios las usarían?
-1. Creación: Basta con agregar el id y la descripción de la tarea que queramos crear y como por arte de magia, tenemos una tarea pendiente por completar.
-2. Seguimiento: Al tener la funcionalidad de ver la tarea como completada o no; los usuarios podrán hacer un seguimiento claro de qué tarea tienen pendiente y qué les falta para realizar parte de su quéhacer diario.
-3. Fácil de usar: no hace falta ser experto en informática para usar la aplicación ¡Cualquier persona puede usarla! y es extremadamente sencillo ¡Anímate!
+## ¿Cuáles son las funcionalidades más importantes y porque los usuarios deberían usarla?
+1. Agregar tareas: Por medio de un indicador único y una descripción, podrás crear una nueva tarea.
+2. Eliminar tareas: Podrás borrar una tarea realizada o irrelevante, proporcionando el indicador de la tarea.
+3. Completar tareas: A través del indicador, podrás completar una tarea cuando ya la hayas realizado.
+4. Mostrar tareas: Podrás ver todas las tareas en una lista, incluyendo la descripción, el indicador y la completitud.
 
-### Instalación
-1. Clonar este repositorio
+## Importante
+La aplicación "corre" en un servidor local y usa un servidor web proporcionado por Node para que siempre sea posible acceder a las tareas por medio del navegador. Así, la aplicación podrá usarse en cualquier dispositivo móvil.
+
+## Beneficios
+1. Organización y Seguimiento de Tareas: Adiós a los problemas de desorganización con esta aplicación. Ahora, puedes crear, eliminar y completar cualquier tarea a cualquiera hora; manteniendo un seguimiento constante de tu día a día.
+2. Fácil Acceso: La ejecución en el servidor permite interactuar con la aplicación y con tus tareas desde cualquier dispositivo con acceso a internet. 
+3. Interfaz intuitiva de línea de comandos: Lo único que deberás hacer es seguir las instrucciones que vayas viendo en pantalla y así podrías ir interactuando con la aplicación.
+
+## Instalación
+
+1. Clonar este repositorio:
+``` 
+git clone <https://github.com/aarroyavez/node-server>
+``` 
+2. Acceder al directorio:
 ```
-https://github.com/aarroyavez/express-server.git
+cd task-list-node
 ```
-2. Pasarse a la rama project-2
-```
-git checkout project-2
-```
-3. Instalar dependencias
+3. Instalar dependencias:
 ```
 npm install nodemon
 ```
 ```
-npm install express
+const readline = require("readline")
 ```
-4. Usar aplicación
 ```
-npm run start
+const http = requiere("http")
 ```
+## Uso
+
+Para poder usar la app, usa el siguiente comando en la terminal:
+```
+node tasks.js
+```
+
 ## Contribución
 Si encuentras algún error, tienes alguna sugerencia o deseas contribuir de alguna forma, siéntete libre de abrir un problema o enviar una solicitud de extracción.
---
+++ tasks.js
@@ -0,0 +1,181 @@
+const readline = require("readline")
+const express = require("express")
+const app = express();
+const port = 8080;
+
+// interfaz para interactuar con el usuario en línea de comandos
+const readlineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+// arreglo que almacenará tareas
+const tasks = []; 
+// solicitudes entrantes en formato json
+app.use(express.json());
+
+// app.get("/tasks", (req, res) => {
+//     res.json(tasks);
+// })
+
+app.get("/tasks/:indicator/:description/:completed", (req, res) => {
+    const indicator = req.params.indicator;
+    const description = req.params.description;
+    const completed = req.params.completed;
+
+    res.json({indicator, description, completed})
+})
+
+app.post("/tasks", (req, res) => {
+    // const body = req.body
+    // res.send(body)
+    const indicator = req.body.indicator;
+    const repeatedTask = tasks.find((task) => task.indicator === indicator);
+    if (repeatedTask) {
+        return res.status(400).json({error: "Indicador inválido"});
+    }
+
+    const description = req.body.description;
+    const task = {
+        indicator,
+        description,
+        completed: false
+    };
+    tasks.push(task);
+
+    res.json({mensaje: "Tarea agregada"});
+});
+
+app.delete("/tasks/:indicator", (req, res) => {
+    const indicator = req.params.indicator;
+    const taskIndex = tasks.findIndex((task) => task.indicator === indicator);
+    if (taskIndex != -1) {
+        tasks.splice(taskIndex, 1);
+        return res.json({mensaje: "Tarea eliminada correctamente"});
+    }
+    res.status(404).json({ error: "No es posible eliminar. Ninguna tarea coincide con el indicador suministrado. Por favor, intente de nuevo."});
+
+});
+
+app.put("/tasks/:indicator", (req, res) => {
+    const indicator = req.params.indicator;
+    const task = tasks.find((task) => task.indicator === indicator);
+    if (task) {
+        task.completed = true;
+        return res.json({mensaje: "Tarea completada"});
+    }
+    res.status(404).json({error: "No es posible completar. Ninguna tarea coincide con el indicador suministrado. Por favor, intente de nuevo."})
+})
+
+app.listen(port, () => {
+    console.log("server listening on port", `${port}`)
+})
+
+const questionAsync = (question) => {
+    return new Promise((resolve) => {
+        readlineInterface.question(question, resolve);
+    });
+};
+
+const promptAddTask = () => {
+    return new Promise(async(resolve) => {
+        const indicator = await questionAsync("Por favor, digite un indicador único para la tarea: "); // await antes de qestionAsyn para esperar la respuesta del usuario
+        if (isNaN(indicator)) {
+            (console.log("El indicador debe ser un número. Por favor, intenta de nuevo"))
+            showMenu();
+            return;
+        }
+        const repeatedTask = tasks.find((task) => task.indicator === indicator);
+        if (repeatedTask) {
+            (console.log("Ya existe una tarea con el mismo indicador. Por favor, intenta con otro número."));
+            showMenu();
+            return;
+        }
+        const description = await questionAsync("Por favor, digite una descripción para la tarea que desee agregar: ");
+        const task = {
+            indicator,
+            description,
+            completed: false
+        };
+        tasks.push(task);
+        console.log("Tarea agregada ✅");
+        resolve();
+        showMenu();
+    });
+};
+
+const promptCompleteTask = () => {
+    return new Promise(async(resolve) => {
+        const indicator = await questionAsync("Por favor, digite el indicador de la tarea a marcar como completada: ")
+        const task = tasks.find((task) => task.indicator === indicator);
+        if (task) {
+            task.completed = true;
+            console.log("Tarea completada ✅");
+        } else {
+            console.log("No es posible completar. Ninguna tarea coincide con el indicador suministrado. Por favor, intente de nuevo.");
+        }
+        resolve();
+        showMenu();
+    });
+};
+
+const promptDeleteTask = () => {
+    return new Promise(async(resolve) => {
+        const indicator = await questionAsync("Por favor, digite un indicador para la tarea que desee eliminar: ");
+        const taskIndex = tasks.findIndex((task) => task.indicator === indicator);
+        if (taskIndex !== -1) {
+            tasks.splice(taskIndex, 1);
+            console.log("Tarea eliminada ✅");
+        } else {
+            console.log("No es posible eliminar. Ninguna tarea coincide con el indicador suministrado. Por favor, intente de nuevo.");
+        }
+        resolve();
+        showMenu();
+    });
+};
+
+const showTasks = () => {   
+    console.log("Task List: ");
+    tasks.forEach((task, index) => {
+        console.log(`[${index}] Indicador: ${task.indicator} | Descripción: ${task.description} | Completada: ${task.completed ? "Sí" : "No"}`);
+    });
+    showMenu();
+};
+
+const showMenu = () => {
+    console.log('\n--- MENU ---');
+    console.log('1. Agregar tarea');
+    console.log('2. Eliminar tarea');
+    console.log('3. Marcar tarea como completada');
+    console.log('4. Mostrar lista de tareas');
+    console.log('5. Salir');
+
+    readlineInterface.question('\nSeleccione una opción: ', (option) => {
+        switch (option) {
+            case "1":
+                promptAddTask();
+                break;
+            case '2':
+                promptDeleteTask();
+                break;
+            case '3':
+                promptCompleteTask();
+                break;
+            case '4':
+                showTasks();
+                break;
+            case '5':
+                readlineInterface.close();
+                break;
+            default:
+                console.log("INVÁLIDO. DEBE INGRESAR ESCOGER UNA DE LAS OPCIONES DEL MENÚ: 1, 2, 3, 4, 5");
+                showMenu();
+                break;
+        }
+    });
+};
+
+console.log("BIENVENIDO A LA APP PARA ADMINISTRAR TUS TAREAS");
+
+showMenu();
+
+
